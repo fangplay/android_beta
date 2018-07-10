ee457e2484ff7d92db84bb3f97fa3987f37ef243
fangplay <fangplay_2014@hotmail.com>
2018-07-10 13:31:55 +0700

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # beta1
 # beta1
 # beta1
+# beta2
--- app/src/main/java/com/protoprogrammer/mylistview/MainActivity.java
+++ app/src/main/java/com/protoprogrammer/mylistview/MainActivity.java
@@ -3,14 +3,16 @@ package com.protoprogrammer.mylistview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
 
-    String[] values = new String[] {
+    String[] values = new String[]{
             "Item 1: Android",
             "Item 2: iPhone ",
             "Item 3: Windows Phone",
@@ -23,10 +25,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.list);
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1 , android.R.id.text1 , values);
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
         listView.setAdapter(adapter);
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String itemValue = (String) listView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + position + "  ListItem : " + itemValue, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 }
